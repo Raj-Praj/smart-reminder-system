cd63ce0464fab14b662d9f59a4c18b7df7b77106
Raj Prajapati <rajpz3479@gmail.com>
2026-05-11 00:56:19 +0545

    fix: secure database credentials using env variables

--- db.php
+++ db.php
@@ -8,15 +8,15 @@ mysqli_real_connect(
     $conn,
     "mysql-29b24fb6-esmart-reminder-system.k.aivencloud.com",
     "avnadmin",
-    "AVNS_FbAJ_XwSUqIgs8eajOg",
+    getenv("DB_PASSWORD"),
     "defaultdb",
     10007,
     NULL,
     MYSQLI_CLIENT_SSL
 );
 
-if ($conn->connect_error) {
-    die("DB Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("Database connection failed");
 }
 
 ?>
