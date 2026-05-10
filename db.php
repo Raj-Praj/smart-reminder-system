f717fa72bd1b92f70bbe1e1860e83371d17b7bae
Raj Prajapati <rajpz3479@gmail.com>
2026-05-11 00:50:09 +0545

    update db.php

--- db.php
+++ db.php
@@ -1,14 +1,22 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "smart_reminder";
+$conn = mysqli_init();
 
-$conn = new mysqli($host, $user, $password, $dbname);
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+mysqli_real_connect(
+    $conn,
+    "mysql-29b24fb6-esmart-reminder-system.k.aivencloud.com",
+    "avnadmin",
+    "AVNS_FbAJ_XwSUqIgs8eajOg",
+    "defaultdb",
+    10007,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
 
 if ($conn->connect_error) {
-    die("Database Connection Failed: " . $conn->connect_error);
+    die("DB Connection failed: " . $conn->connect_error);
 }
 
 ?>
