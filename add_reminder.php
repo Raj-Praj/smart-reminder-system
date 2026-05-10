90cf62a0fc538170cd5903d1d21fa8e0e62fc7b0
Raj Prajapati <rajpz3479@gmail.com>
2026-05-11 01:56:04 +0545

    header issue fix

--- add_reminder.php
+++ add_reminder.php
@@ -17,11 +17,9 @@ if ($_POST) {
     $early = $_POST['early_reminder_minutes'] ?: NULL;
     $repeat_type = $_POST['repeat_type'];
     $user_id = $_SESSION['user_id'];
-    $early_minutes = $_POST['early_reminder_minutes'];
+    $early_minutes = $_POST['early_reminder_minutes']?:NULL;
 
-    if ($early_minutes === "" || $early_minutes === null) {
-        $early_minutes = "NULL";
-    }
+   
 
     $sql = "INSERT INTO reminders 
     (user_id, title, description, email, final_time, early_reminder_minutes,repeat_type)
